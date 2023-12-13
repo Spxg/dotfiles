8ae8f6b1e4ac30df73e8615cdc62e9bb0c29c86b
Spxg <unsafe@outlook.es>
2023-12-14 00:13:40 +0800

    update keymaps

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -8,20 +8,8 @@ vim.keymap.set("n", "<leader>p", function()
 end)
 
 -- flash.nvim
-vim.keymap.set({ "n", "o", "x" }, "s", function()
+vim.keymap.set({ "n", "o", "x" }, "<leader>j", function()
   require("flash").jump()
-end)
-vim.keymap.set({ "n", "o", "x" }, "S", function()
-  require("flash").treesitter()
-end)
-vim.keymap.set("o", "r", function()
-  require("flash").remote()
-end)
-vim.keymap.set({ "o", "x" }, "R", function()
-  require("flash").treesitter_search()
-end)
-vim.keymap.set("c", "<c-s>", function()
-  require("flash").toggle()
 end)
 
 -- Comment.nvim
