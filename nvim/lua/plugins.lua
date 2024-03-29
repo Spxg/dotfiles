11434b848050ac8f40f996a1693be2891907bbb4
Spxg <unsafe@outlook.es>
2024-03-30 00:50:07 +0800

    add nvim-surround

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -136,4 +136,15 @@ return {
       -- options
     },
   },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      ---@diagnostic disable-next-line: missing-fields
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end,
+  },
 }
