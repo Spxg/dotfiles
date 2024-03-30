a004597dd1bd40b241bf8c3eee4e4be1ba6c8a48
Spxg <unsafe@outlook.es>
2024-03-31 01:53:13 +0800

    add indent-blankline.nvim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -139,4 +139,16 @@ return {
       })
     end,
   },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    config = function()
+      require("ibl").setup({
+        scope = {
+          show_start = false,
+          show_end = false,
+        },
+      })
+    end,
+  },
 }
