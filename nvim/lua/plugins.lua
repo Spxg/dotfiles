255a2b33903c3f494ceb8ece418b85ddc0b79108
Spxg <unsafe@outlook.es>
2024-03-30 16:37:52 +0800

    update fzf-lua

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -42,7 +42,6 @@ return {
     "ibhagwan/fzf-lua",
     -- optional for icon support
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    url = "https://gitlab.com/ibhagwan/fzf-lua",
     config = function()
       -- calling `setup` is optional for customization
       require("fzf-lua").setup()
@@ -140,5 +139,4 @@ return {
       })
     end,
   },
-
 }
