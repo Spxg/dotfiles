58bc65210f77b1db49499f77cf7318a5b2f4c4bb
Spxg <unsafe@outlook.es>
2024-03-31 02:03:01 +0800

    add arrow.nvim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -151,4 +151,11 @@ return {
       })
     end,
   },
+  {
+    "otavioschwanck/arrow.nvim",
+    opts = {
+      show_icons = true,
+      leader_key = ";", -- Recommended to be a single key
+    },
+  },
 }
