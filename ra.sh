7a736184d646650e641444af16f38a77e0c7b5d0
Spxg <unsafe@outlook.es>
2024-07-12 13:24:08 +0800

    remove unused script

--- ra.sh
+++
@@ -1,3 +0,0 @@
-mkdir -p ~/.local/bin
-curl -L https://github.com/rust-lang/rust-analyzer/releases/latest/download/rust-analyzer-$TARGET.gz | gunzip -c - > ~/.local/bin/rust-analyzer
-chmod +x ~/.local/bin/rust-analyzer
